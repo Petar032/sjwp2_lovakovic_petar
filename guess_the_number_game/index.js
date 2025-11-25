556a72c1742a7881b6c1942d2093a9824f4d06e2
Petar032 <petarlovakovic222@gmail.com>
2025-11-25 12:00:22 +0100

    xbv

--- guess_the_number_game/index.js
+++ guess_the_number_game/index.js
@@ -4,19 +4,44 @@ function randomNumber(){
 let input = document.getElementById("guess");
 let msg = document.getElementById("msg");
 let attempts= document.getElementById("attempts");
-let randomNumber = randomNumber();
+let randNumber = randomNumber();
 let pokusaji = 0;
 
 function updateAttempts(){
-  let pokusaji=0;
   attempts.innerHTML = "Pokušaji: " + pokusaji;
 
 }
 updateAttempts();
 function CheckNumber(event){
     event.preventDefault(); // ne osvježava stranicu
+    let value = parseInt(input.value);
+    /*console.log(value);*/
+    if(isNaN(value) || value < 1 || value > 100){
+      msg.innerHTML = "Unesite broj između 1 i 100.";
+      msg.style.color = "crimson";
+      return;
+    }
+    if(value === randNumber){
+      msg.innerHTML = "Pogodak. Broj je " + randNumber;
+      msg.style.color = "green";
+    }else if(value < randNumber){
+      msg.innerHTML = "Premalo";
+      msg.style.color = "orange";
+    }else{
+      msg.innerHTML = "Previše";
+      msg.style.color = "orange";
+    }
+    pokusaji++;
+    updateAttempts();
+
     
 }
 function NewGame(){
+  randNumber = randomNumber();
+  pokusaji = 0;
+  updateAttempts();
+  msg.innerHTML = "Nova igra - pokušaj ponovno.";
+  msg.style.color = "black";
+  input.focus();
    
 }
